d380d60b3e5149c71c6df1cdda04f0767de31723
Luiz Felipe <luizfpereira406@gmail.com>
2022-03-07 16:13:19 -0300

    quinto exercico switch-case

--- FundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/if-else.js
+++ FundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/if-else.js
@@ -3,7 +3,7 @@ if (notaDesafio >= 80) {
     console.log("Parabéns, você foi aprovada(o)!");
 }
 else if (notaDesafio < 80 && notaDesafio >= 60) {
-    console.log("Você está na nossa lista de espera")
+    console.log("Você está na nossa lista de espera");
 }
 else{
     console.log("Você foi Reprovada(o)");
