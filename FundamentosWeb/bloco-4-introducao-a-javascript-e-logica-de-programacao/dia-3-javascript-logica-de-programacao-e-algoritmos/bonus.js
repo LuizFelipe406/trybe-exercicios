62f508ce62de8c8bf5e90e323851c7b9a2181c55
Luiz Felipe <luizfpereira406@gmail.com>
2022-03-09 19:23:46 -0300

    exercicios bonus dia 4.3

--- FundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
+++ FundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -20,15 +20,70 @@
 // }
 
 // 3- Agora inverta o lado do triângulo. 
-let n = 5;
-for (let index = 0; index < n; index += 1) {
-    let asteriscos = "";
-    let espacos = "";
-    for (let index3 = n - 1; index3 > index; index3 -= 1) {
-        espacos += " ";
+// let n = 5;
+// for (let index = 0; index < n; index += 1) {
+//     let asteriscos = "";
+//     let espacos = "";
+//     for (let index3 = n - 1; index3 > index; index3 -= 1) {
+//         espacos += " ";
+//     }
+//     for (let index2 = 0; index2 <= index; index2 += 1) {
+//         asteriscos += "*";
+//     }
+//     console.log(espacos,asteriscos);
+// }
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+// let n = 5;
+// for (let index = 0; index < n; index += 2) {
+//     let asteriscos = "";
+//     let espacos = "";
+//     for (let index3 = n - 1; index3 > index; index3 -= 2) {
+//         espacos += " ";
+//     }
+//     for (let index2 = 0; index2 <= index; index2 += 1) {
+//         asteriscos += "*";
+//     }
+//     console.log(espacos,asteriscos);
+// }
+
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+// let n = 7;
+// for (let index = 0; index < n; index += 2) {
+//     let asteriscos = "";
+//     let espacos = "";
+//     for (let index3 = n - 1; index3 > index; index3 -= 2) {
+//         espacos += " ";
+//     }
+//     if (index == n - 1) {
+//         for (let index2 = 0; index2 <= index; index2 += 1) {
+//             asteriscos += "*";
+//         }
+//     }
+//     else {
+//         for (let index2 = 0; index2 <= index; index2 += 1) {
+//             if (index2 == 0 || index2 == index) {
+//                 asteriscos += "*";
+//             }
+//             else {
+//                 asteriscos += " ";
+//             }
+//         }
+//     }
+//     console.log(espacos, asteriscos);
+// }
+
+//6- Faça um programa que diz se um número definido numa variável é primo ou não.
+let numero = 0;
+let contadorDeDivisores = 0;
+for (let index = 1; index <= numero; index += 1) {
+    if (numero % index == 0) {
+        contadorDeDivisores += 1
     }
-    for (let index2 = 0; index2 <= index; index2 += 1) {
-        asteriscos += "*";
-    }
-    console.log(espacos,asteriscos);
+}
+if (contadorDeDivisores <= 2 && contadorDeDivisores !== 0) {
+    console.log("O numero", numero, "é primo!");
+}
+else {
+    console.log("O numero", numero, "não é primo!");
 }
