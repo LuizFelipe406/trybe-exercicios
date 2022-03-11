d84527ade6e00f903dff0d4e8b1b046bbccd3e64
Luiz Felipe <luizfpereira406@gmail.com>
2022-03-11 12:26:06 -0300

    romanos

---
+++ FundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objeto-e-funcoes/bonus.js
@@ -0,0 +1,42 @@
+// 1 - (Difícil) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+function romano(string) {
+    let conversao = [];
+    let resultado = [];
+    let soma = 0;
+    let tabela = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    }
+    for (index in string) {
+        for (i in tabela) {
+            if (string[index] == i) {
+                conversao.push(tabela[i])
+            }
+        }
+    }
+    for (let i = 0; i < conversao.length; i += 1) {
+        if (i == 0 || conversao[i] == conversao[i-1]) {
+            resultado.push(conversao[i]);
+        } else if (conversao[i] < conversao[i + 1]) {
+            resultado.push(conversao[i + 1] - conversao[i])
+            i+=1
+        }else if (conversao[i] >= conversao[i+1]){
+            resultado.push(conversao[i]+conversao[i+1])
+            i+=1
+        }else if (i==conversao.length-1){
+            resultado.push(conversao[i]);
+        }
+    }
+    for(i in resultado){
+soma += resultado[i];
+    }
+    console.log(soma);
+}
+
+romano("XXIX");
