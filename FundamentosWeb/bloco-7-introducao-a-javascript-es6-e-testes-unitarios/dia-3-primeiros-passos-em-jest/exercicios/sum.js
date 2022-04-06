cf6ce31797b280d996fc5a91a04ca956ec6d382d
Luiz Felipe <luizfpereira406@gmail.com>
2022-04-06 11:18:32 -0300

    exercicio 4 a 6

--- FundamentosWeb/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.js
+++ FundamentosWeb/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.js
@@ -17,15 +17,82 @@ function myRemove(arr, item) {
 }
 
 function myFizzBuzz(num) {
-    if (typeof num !== 'number') return false;
-    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-    if (num % 3 === 0) return 'fizz';
-    if (num % 5 === 0) return 'buzz';
-    return num;
+  if (typeof num !== "number") return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
+  return num;
+}
+
+function encode(string) {
+  let newWord = string.split("");
+  let codigo = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  for (let i in string) {
+    for (let key in codigo) {
+      if (string[i] === key) {
+        newWord[i] = codigo[key];
+      }
+    }
   }
-  
-  // implemente seus testes aqui
+  return newWord.join("");
+}
+
+function decode(string) {
+  let newWord = string.split("");
+  let codigo = {
+    1: "a",
+    2: "e",
+    3: "i",
+    4: "o",
+    5: "u",
+  };
+  for (let i in string) {
+    for (let key in codigo) {
+      if (string[i] === key) {
+        newWord[i] = codigo[key];
+      }
+    }
+  }
+  return newWord.join("");
+}
+
+const techList = (array, nome) => {
+  const arrayOrdenated = array.sort();
+  if(array.length === 0){
+    return "Vazio!";
+  }
+  const lista = [];
+  for (let i = 0; i < arrayOrdenated.length; i += 1){
+    const obj = {
+      tech: arrayOrdenated[i],
+      name: nome,
+    }
+    lista.push(obj);
+  }
+  return lista;
+}
+
+const hydrate = (string) => {
+  const stringArray = string.match(/\d+/g);
+  let quant = 0;
+  const numbers = [];
+  for (let i = 0; i < stringArray.length; i += 1) {
+    numbers.push(parseInt(stringArray[i], 10));
+  }
+  for (let i = 0; i < numbers.length; i += 1) {
+    quant += numbers[i];
+  } if (quant === 1) {
+    return `${quant} copo de água`;
+  } return `${quant} copos de água`;
+}
+
 
 // implemente seus testes aqui
 
-module.exports = {sum, myRemove, myFizzBuzz};
+module.exports = { sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate };
