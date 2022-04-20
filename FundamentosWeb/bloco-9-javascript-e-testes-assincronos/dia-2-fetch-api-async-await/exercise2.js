f2270fec8edea6a7e334142477272159f541d243
Luiz Felipe <luizfpereira406@gmail.com>
2022-04-20 16:22:11 -0300

    exercicio bonus

--- FundamentosWeb/bloco-9-javascript-e-testes-assincronos/dia-2-fetch-api-async-await/exercise2.js
+++ FundamentosWeb/bloco-9-javascript-e-testes-assincronos/dia-2-fetch-api-async-await/exercise2.js
@@ -2,18 +2,23 @@ const API_URL = 'https://api.coincap.io/v2/assets';
 const criptoList = document.querySelector('#criptosList');
 const fetchCripto = () => {
   fetch(API_URL)
-  .then(response => response.json())
-  .then(object => {
-    const top10 = object.data.filter(data => parseInt(data.rank) < 11);
-    top10.forEach(cripto => {
-      const text = `${cripto.name} (${cripto.symbol}): ${parseFloat(cripto.priceUsd).toFixed(2)}`;
-      const list = document.createElement('li');
-      list.innerText = text;
-      criptoList.appendChild(list);
-    })
-  }).catch(error => {
-    console.error(error);
-  });
+    .then(response => response.json())
+    .then(object => {
+      const top10 = object.data.filter(data => parseInt(data.rank) < 11);
+      top10.forEach(cripto => {
+        fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json')
+          .then(response => response.json())
+          .then((currencies) => {
+            const priceInBrl = (currencies.usd.brl * parseFloat(cripto.priceUsd)).toFixed(2);
+            const text = `${cripto.name} (${cripto.symbol}): ${priceInBrl}`;
+            const list = document.createElement('li');
+            list.innerText = text;
+            criptoList.appendChild(list);
+          });
+      })
+    }).catch(error => {
+      console.error(error);
+    });
 };
 
 window.onload = () => fetchCripto();
