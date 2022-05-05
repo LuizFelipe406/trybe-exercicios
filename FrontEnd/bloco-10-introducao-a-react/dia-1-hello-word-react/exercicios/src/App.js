164cbd0031582bcb6cb275e5c1f5e2284ee237e3
Luiz Felipe <luizfpereira406@gmail.com>
2022-05-05 18:05:54 -0300

    exercicio 2 - lista de tasks

--- FrontEnd/bloco-10-introducao-a-react/dia-1-hello-word-react/exercicios/src/App.js
+++ FrontEnd/bloco-10-introducao-a-react/dia-1-hello-word-react/exercicios/src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
 
-function App() {
+const tasks = ['fazer os exercicios', 'ir para academia', 'trabalhar'];
+
+const Task = (array) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    array.map(task => <li>{task}</li>)
   );
+};
+
+function App() {
+  return Task(tasks);
 }
 
 export default App;
