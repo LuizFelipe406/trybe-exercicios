7d84bda2fada45cd25150e657b453c61bd20f2d3
Luiz Felipe <luizfpereira406@gmail.com>
2022-05-10 15:56:09 -0300

    Exercicio de fixação - Adicionando um contador nos Botões evento e estado

--- FrontEnd/bloco-11-componentes-com-estado-evento-e-formularios-react/dia-1-componentes-com-estado-e-evento/fancy-buttons/src/button.js
+++ FrontEnd/bloco-11-componentes-com-estado-evento-e-formularios-react/dia-1-componentes-com-estado-e-evento/fancy-buttons/src/button.js
@@ -4,18 +4,26 @@ class Button extends React.Component {
 
   constructor () {
     super();
+    this.state = {
+      'Primeiro Botão':0,
+      'Segundo Botão':0,
+      'Terceiro Botão':0,
+    }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick () {
-    const { buttonMessage } = this.props;
+    const { buttonMessage, buttonText } = this.props;
     console.log(buttonMessage);
+    this.setState((estadoAnterior, _props) => ({
+      [buttonText]: estadoAnterior[buttonText] + 1
+    }))
   }
 
   render() {
     const { buttonText } = this.props;
     return (
-      <button onClick={this.handleClick}>{buttonText}</button>
+      <button onClick={this.handleClick}>{`${buttonText}:${this.state[buttonText]}`}</button>
     )
   }
 }
