06e0cc14a5826f1c6dd73fe83637b61ad2ead881
Luiz Felipe <luizfpereira406@gmail.com>
2022-05-10 15:33:28 -0300

    exercicio de fixação - this e bind

--- FrontEnd/bloco-11-componentes-com-estado-evento-e-formularios-react/dia-1-componentes-com-estado-e-evento/fancy-buttons/src/button.js
+++ FrontEnd/bloco-11-componentes-com-estado-evento-e-formularios-react/dia-1-componentes-com-estado-e-evento/fancy-buttons/src/button.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-function handleClick () {
-  console.log('clicou')
-}
-
 class Button extends React.Component {
+
+  constructor () {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    const { buttonMessage } = this.props;
+    console.log(buttonMessage);
+  }
+
   render() {
     const { buttonText } = this.props;
     return (
-      <button onClick={handleClick}>{buttonText}</button>
+      <button onClick={this.handleClick}>{buttonText}</button>
     )
   }
 }
