df7d6007ef2a7278299046c86e2b120376a4fb84
LuizFelipe406 <luizfpereira406@gmail.com>
2022-08-23 18:49:22 -0300

    Conteudos do dia 22.3

---
+++ BackEnd/bloco-22-node-js/dia-3-testes-de-integracao/cacau-trybe/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cacauTrybe = require('./cacauTrybe');
+
+const app = express();
+
+app.get('/chocolates', async (req, res) => {
+  const chocolates = await cacauTrybe.getAllChocolates();
+  res.status(200).json({ chocolates });
+});
+
+app.get('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+  const chocolate = await cacauTrybe.getChocolateById(Number(id));
+  res.status(200).json({ chocolate });
+});
+
+app.get('/chocolates/brand/:brandId', async (req, res) => {
+  const { brandId } = req.params;
+  const chocolates = await cacauTrybe.getChocolatesByBrand(Number(brandId));
+  res.status(200).json({ chocolates });
+});
+
+module.exports = app;
