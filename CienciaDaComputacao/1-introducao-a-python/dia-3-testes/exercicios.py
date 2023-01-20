4c47163048d7083610bd6b12af7c812b49e5b827
LuizFelipe406 <luizfpereira406@gmail.com>
2023-01-20 18:23:20 -0300

    Exercicio 2 - 4.1.3

--- CienciaDaComputacao/1-introducao-a-python/dia-3-testes/exercicios.py
+++ CienciaDaComputacao/1-introducao-a-python/dia-3-testes/exercicios.py
@@ -15,3 +15,25 @@ def exercicio_1 (list):
   except TypeError:
     raise TypeError
   return result
+
+def exercicio_2 (string: str):
+  conversor = {
+    "ABC":2,
+    "DEF":3,
+    "GHI":4,
+    "JKL":5,
+    "MNO":6,
+    "PQRS":7,
+    "TUV":8,
+    "WXYZ":9,
+  }
+  result = ''
+  for letter in string:
+    for key in conversor:
+      if letter.upper() in list(key):
+        result += str(conversor[key])
+    if letter == '-' or letter.isdigit():
+      result += letter
+  return result
+
+print(exercicio_2("1-HOME-SWEET-HOME"))
